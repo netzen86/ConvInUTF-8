ffd369f8fa593bd6090978a1ee7890de2c0f9a3e
netzen86 <netzen0su@gmail.com>
2021-05-20 22:56:01 +0500

    Script complete

--- decode_encode.py
+++ decode_encode.py
@@ -1,21 +1,45 @@
 #!/usr/bin/python3
 from sys import argv
+from sys import exit
+import subprocess 
 
 def decuniescseq(infile):
-    convert = []
+    dectxt = []
     with open(infile, encoding='utf-8') as f:
         for line in f:
             line = line.encode("unicode_escape").replace(b'\\\\u', b'\\u').decode("unicode_escape")
-            convert.append(line)
+            dectxt.append(line)
+    return dectxt
+
+def encescseq(infile):
+    enctxt = []
+    with open(infile, encoding='utf-8') as f:
+        for line in f:
+            line = line.encode("unicode_escape").decode("utf-8").replace("\\n", "\n")
+            enctxt.append(line)
+    return enctxt
+
 
 
 
 if __name__ == "__main__":
-    outfile = argv[1]
+    try:
+        action = argv[1]
+        inputf = argv[2]
+        outputf = argv[3]
+    except IndexError:
+        print("Run script with this args:\nAction - decode | encode; input file; output file")
+        exit(1)
 
-    f = open(outfile, 'w')
-    f.writelines(convert)
-    f.close()
+    print(f'\n*** {inputf} is backuped ***\n')
+    subprocess.run(['cp', inputf, inputf + '.bak'])    
     
-    f = open(convOutfile, 'r')
+    f = open(outputf, 'w')
+    if "decode" in action:
+        f.writelines(decuniescseq(inputf))
+    elif "encode" in action:
+        f.writelines(encescseq(inputf))
+
+    f.close()
+    f = open(outputf, 'r')
     print(f.read())
